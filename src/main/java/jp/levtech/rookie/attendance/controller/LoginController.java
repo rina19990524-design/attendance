7e0b83e59a9973f21242f38996d816c5f636eab3
rina19990524-design <rina19990524@gmail.com>
2026-09-14 18:41:14 +0900

    loginControllerに不要なコードが入っていたため、削除

--- src/main/java/jp/levtech/rookie/attendance/controller/LoginController.java
+++ src/main/java/jp/levtech/rookie/attendance/controller/LoginController.java
@@ -3,31 +3,19 @@ package jp.levtech.rookie.attendance.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import jp.levtech.rookie.attendance.repository.TestRepository;
-
 /**
- * ホーム画面を管理するコントローラー
+ * ログイン画面を表示するコントローラー
  */
 @Controller
 public class LoginController {
-	
-	private final TestRepository testRepository;
-	
-	public LoginController (TestRepository testRepository ) {
-		
-		this.testRepository = testRepository;
-		
-	}
 
-	/**
-	 * ログイン画面を扱う。
-	 *
-	 * @return テンプレート
-	 */
-	@GetMapping("/login")
-	public String login() {
-		// レンダリングに利用するテンプレート名を返す。
-		return "login";
-	}
-
+    /**
+     * ログイン画面を表示する
+     *
+     * @return ログイン画面のテンプレート名
+     */
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 }
